f8f52307e7fcd4e34ce7d3bdd01c9a9abda07fd0
klhkassel <klhkassel@gmail.com>
2022-12-10 13:55:52 -0500

    added vaccine info section to medical page

--- capstone-project/src/pages/medical/Medical.js
+++ capstone-project/src/pages/medical/Medical.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Notes from "./Notes";
+import VaccineInfo from "./VaccineInfo";
 
 function Medical() {
   return (
     <div className="medical">
       <h1>Medical</h1>
+      <VaccineInfo />
       <Notes />
     </div>
   );
